b6ec6d1cca3a91e71fed83cf1c8a454f4ca37af6
Andy Brudtkuhl <abrudtkuhl@gmail.com>
2013-04-19 12:08:54 -0500

    dwolla integration

--- crutch.js
+++ crutch.js
@@ -1,6 +1,6 @@
 if (Meteor.isClient) {
-  Template.splash.greeting = function () {
-    return "Welcome to crutch.";
+  Template.splash.title = function () {
+    return "Cover My Crutches";
   };
 }
 
